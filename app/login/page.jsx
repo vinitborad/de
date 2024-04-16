e80d76ea95c017b9b7eb846a4112140bf22096d3
vinitborad <vinitborad12@gmail.com>
2024-04-16 11:25:33 +0530

    initial setup

---
+++ app/login/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import './index.css'
+
+export default function Home() {
+  return (
+    <div>
+        <h1 className=" text-white">hi laodngn </h1>
+    </div>
+  );
+}
